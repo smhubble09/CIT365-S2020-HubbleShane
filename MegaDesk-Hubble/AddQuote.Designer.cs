a845bba6e63f203b9f76865313e15208994bab1a
Shane Hubble <smhubble09@byui.edu>
2020-05-13 13:14:36 -0600

    Commit

--- MegaDesk-Hubble/AddQuote.Designer.cs
+++ MegaDesk-Hubble/AddQuote.Designer.cs
@@ -148,7 +148,7 @@ namespace MegaDesk_Hubble
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(29, 26);
+            this.label2.Location = new System.Drawing.Point(17, 26);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(91, 20);
             this.label2.TabIndex = 5;
@@ -158,7 +158,7 @@ namespace MegaDesk_Hubble
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(29, 58);
+            this.label3.Location = new System.Drawing.Point(17, 60);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(94, 20);
             this.label3.TabIndex = 7;
@@ -280,9 +280,9 @@ namespace MegaDesk_Hubble
             // 
             this.WidthBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.errorProvider1.SetIconPadding(this.WidthBox, -65);
-            this.WidthBox.Location = new System.Drawing.Point(129, 21);
+            this.WidthBox.Location = new System.Drawing.Point(132, 21);
             this.WidthBox.Name = "WidthBox";
-            this.WidthBox.Size = new System.Drawing.Size(49, 23);
+            this.WidthBox.Size = new System.Drawing.Size(46, 23);
             this.WidthBox.TabIndex = 1;
             this.WidthBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.WidthBox.TextChanged += new System.EventHandler(this.WidthBox_TextChanged);
@@ -294,9 +294,9 @@ namespace MegaDesk_Hubble
             // 
             this.DepthBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.errorProvider2.SetIconPadding(this.DepthBox, -65);
-            this.DepthBox.Location = new System.Drawing.Point(129, 58);
+            this.DepthBox.Location = new System.Drawing.Point(132, 58);
             this.DepthBox.Name = "DepthBox";
-            this.DepthBox.Size = new System.Drawing.Size(49, 23);
+            this.DepthBox.Size = new System.Drawing.Size(46, 23);
             this.DepthBox.TabIndex = 2;
             this.DepthBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.DepthBox.TextChanged += new System.EventHandler(this.DepthBox_TextChanged);
@@ -317,7 +317,7 @@ namespace MegaDesk_Hubble
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(29, 88);
+            this.label10.Location = new System.Drawing.Point(17, 88);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(40, 20);
             this.label10.TabIndex = 21;
