ab782ef7fe9ac7cdb95e7ebc55da98f045923dbf
smhubble09 <smhubble09@byui.edu>
2020-05-26 20:32:54 -0600

    Commit

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,18 +1,33 @@
-﻿using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RazorPagesMovie.Models {
     public class Movie {
         public int ID { get; set; }
+
+        //------------------------
+        [StringLength(60, MinimumLength = 3), Required]
         public string Title { get; set; }
 
-        [DataType(DataType.Date)]
+        //------------------------
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
+
+        //------------------------
+        [Range(1, 100), DataType(DataType.Currency), Column(TypeName ="decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        //------------------------
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required]
+        public string Genre { get; set; }
+
+        //------------------------
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), Required, StringLength(5)]
+        public string Rating { get; set; }
     }
 }
--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie {
     public class Startup {
@@ -20,6 +22,9 @@ namespace RazorPagesMovie {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddRazorPages();
+
+    services.AddDbContext<RazorPagesMovieContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("RazorPagesMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
