3e38ace46347223ee8ce4519f19bbf22b1cd8b02
smhubble09 <smhubble09@byui.edu>
2020-06-15 18:03:28 -0600

    Initial Commit

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models {
+    public class Movie {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3), Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$"), StringLength(30), Required]
+        public string Genre { get; set; }
+
+        [Range(1, 100), Column(TypeName = "decimal(18, 2)"), DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+    }
+}
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models {
+    public static class SeedData {
+        public static void Initialize(IServiceProvider serviceProvider) {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>())) {
+                // Look for any movies.
+                if (context.Movie.Any()) {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie {
+                        Title = "The Other Side of Heaven",
+                        ReleaseDate = DateTime.Parse("2002-4-12"),
+                        Genre = "Drama",
+                        Rating = "PG",
+                        Price = 2.99M
+                    },
+
+                    new Movie {
+                        Title = "The Other Side of Heaven 2: Fire of Faith",
+                        ReleaseDate = DateTime.Parse("2019-6-28"),
+                        Genre = "Drama",
+                        Rating = "PG-13",
+                        Price = 3.99M
+                    },
+
+                    new Movie {
+                        Title = "The R.M.",
+                        ReleaseDate = DateTime.Parse("2003-1-31"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 4.99M
+                    },
+
+                    new Movie {
+                        Title = "The Fighting Preacher",
+                        ReleaseDate = DateTime.Parse("2019-7-24"),
+                        Genre = "History",
+                        Rating = "PG",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
