84a92a4c0d60d1ec7f342590401ad06dd3e267c9
smhubble09 <smhubble09@byui.edu>
2020-06-30 18:46:42 -0600

    Initial Commit

---
+++ ContosoUniversity/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models {
+    public abstract class Person {
+        public int ID { get; set; }
+
+        [Required, StringLength(50), Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required, Column("FirstName"), Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        public string FirstMidName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName {
+            get {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models {
+    public class Student {
+        public int ID { get; set; }
+
+        [Required, StringLength(50), RegularExpression(@"^[A-Z]+[a-zA-Z]*$"), Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required, StringLength(50), RegularExpression(@"^[A-Z]+[a-zA-Z]*$"), Column("FirstName")]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true), Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName {
+            get {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
